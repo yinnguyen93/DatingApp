263439f1a06ad90bb5485c51ad6956d1e09e6291
bennguyen <yinnguyen@rocketmail.com>
2022-05-26 13:18:53 -0500

    Client Initial Commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "./_models/user";
+import {AccountService} from "./_services/account.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+
+  constructor(private accountService: AccountService) {
+  }
+
+  ngOnInit() {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AccountService} from "../_services/account.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    if (this.model) {
+      this.accountService.register(this.model).subscribe({
+        next: response => {
+          console.log(response);
+          this.cancel();
+        },
+        error: error => console.log(error)
+      });
+    }
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
